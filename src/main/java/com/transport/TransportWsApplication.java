d4487f4db33af7fbc3ddc1314b06ad06b0f07a24
Shaik Sajid <shaiksajid492@gmail.com>
2022-01-23 00:49:57 +0530

    fixed package scanning

--- src/main/java/com/transport/TransportWsApplication.java
+++ src/main/java/com/transport/TransportWsApplication.java
@@ -1,9 +1,10 @@
-package com.transport.transportws;
+package com.transport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class TransportWsApplication {
 
 	public static void main(String[] args) {
