24ec6f2c8a149ce12ad97435fb2a1adbde3d689f
Shaik Sajid <shaiksajid492@gmail.com>
2022-01-23 00:56:04 +0530

    un used imports

--- src/main/java/com/transport/repository/ClientDetailsDao.java
+++ src/main/java/com/transport/repository/ClientDetailsDao.java
@@ -1,8 +1,8 @@
 package com.transport.repository;
 
-import com.transport.model.ClientDetails;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.transport.model.ClientDetails;
 
 public interface ClientDetailsDao extends JpaRepository<ClientDetails, Integer> {
     
